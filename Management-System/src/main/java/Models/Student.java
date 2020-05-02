0b9de74012ef1ae7056dcbb6f8b19a13c9b5aeb0
Tharnickanth Manoranjan <tharnimo@gmail.com>
2020-05-02 11:28:45 +0530

    delete unnecessary variable

--- Management-System/src/main/java/Models/Student.java
+++ Management-System/src/main/java/Models/Student.java
@@ -6,7 +6,7 @@ public class Student extends NewStudent {
     public String CourseName;
     public String code;
     public String Batch;
-    public String DegreeName;
+   
 
 
     public Student(String SID, String CourseName, String code, String Batch, String fName, String lName, String Address, String District, String NIC, String DOB, String Email, String tNo) {
