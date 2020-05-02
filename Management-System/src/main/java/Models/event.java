fd517f4d072818a9bf8afb97f6e5300eea965818
Tharnickanth Manoranjan <tharnimo@gmail.com>
2020-05-02 14:51:43 +0530

    Create "event" parent class for Handling new event

---
+++ Management-System/src/main/java/Models/event.java
@@ -0,0 +1,15 @@
+package Models;
+
+public class event {
+
+    public String ename;
+    public String date;
+
+    public event(String ename, String date) {
+        this.ename = ename;
+        this.date = date;
+    }
+    public void updateEvent(){
+    
+    }
+}
