c6374b8b39d1f3d33d24313c59891e4e956a0d8f
Tharnickanth Manoranjan <tharnimo@gmail.com>
2020-06-02 03:39:45 +0530

    Create Event.java class

--- Management-System/src/main/java/Models/Event.java
+++ Management-System/src/main/java/Models/Event.java
@@ -1,15 +1,26 @@
 package Models;
 
-public class event {
+public class Event {
 
     public String ename;
-    public String date;
-
-    public event(String ename, String date) {
-        this.ename = ename;
-        this.date = date;
-    }
-    public void updateEvent(){
+    public String eID;
+    public String Description; 
+    public String Message;
+    public String Eventdate;
+    public String StartTime;
+    public String EndTime;  
+    public String Venue;
     
+
+     public Event(String ename,String eID,String Description,String Message,String date,String StartTime,String EndTime,String Venue)
+    {
+       this.ename=ename;
+       this.eID=eID;
+       this.Description=Description;
+       this.Message=Message;
+       this.Eventdate=date;
+       this.StartTime=StartTime;
+       this.EndTime=EndTime;
+       this.Venue=Venue;
     }
 }
